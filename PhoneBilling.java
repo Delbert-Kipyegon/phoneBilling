cb78f786f7e29f802fa0fbfddada2e1cd7338da5
Delbert-Kipyegon <delbertkip@gmail>
2022-11-03 00:06:04 +0300

    Java Update

--- PhoneBilling.java
+++ PhoneBilling.java
@@ -1,32 +1,33 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.regex.Matcher; // show the pattern 
+import java.util.regex.Pattern; // what is to be matched against the pattern
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-// import java.time.LocalTime;
 
 public class PhoneBilling {
 
-    public static void main(String[] args, double cost){
+    public static void main(String[] args){
 
         // declare the required variables
         String number;
         double  duration;
+        double totalCost;
+
 
         // get total talk time 
         duration = 4;
 
         // get number 
-        number = "072961634";
+        number = "072961634"; // safaricom
 
         
         // get current time and hours
         ZoneId zoneId = ZoneId.systemDefault();
         ZonedDateTime time = ZonedDateTime.now(zoneId);
         int hours = time.getHour();
-        System.out.println(hours);
+        System.out.println( hours + ":00 Hrs ");
 
 
-        // create regex to check number format
+        // create regex to check number format of network
         String regex = "07[012459]";
         Pattern  testRgx = Pattern.compile( regex) ;   
         Matcher matcher = testRgx.matcher(number); 
@@ -37,28 +38,58 @@ public class PhoneBilling {
          System.out.println("Calling other Network");
         }
         
-        
 
+        
         // if call is to same network then uses the time to find cost
         // else uses a constant number
         if (matchFound){
-            // LocalTime Six_AM = LocalTime.parse("06:00:00");
-            // LocalTime Six_PM = LocalTime.parse("18:00:00");
-        
             
             // check the hours condition
+            System.out.println("Should check for hours");
+            
             if (hours >= 06 && hours <= 18){
-                 cost = duration * 4;
+                 totalCost = duration * 4;
+                System.out.println("Day");
+
+                // including VAT
+                if(duration > 2){
+                    totalCost *= 1.16;
+                    System.out.println("Your call has exceeded 2 minutes");
+                    System.out.println("The Total Cost is " + totalCost);
+                }else{
+                    System.out.println("The Total Cost is " + totalCost);
+                }
+                System.out.println(totalCost);
             }
                 else{
-                cost = duration * 3;
+                totalCost = duration * 3;
+                System.out.println("Night");
+
+                    // including VAT
+                    if(duration > 2){
+                        totalCost *= 1.16;
+                        System.out.println("Your call has exceeded 2 minutes");
+                        System.out.println("The Total Cost is " + totalCost);
+                    }else{
+                        System.out.println("The Total Cost is " + totalCost);
+                    }
             }
         } else{
-         cost = duration * 5;
+        System.out.println("Should skip hrs check");
+        totalCost = duration * 5;
+         // including VAT
+         if(duration > 2){
+            totalCost *= 1.16;
+            System.out.println("Your call has exceeded 2 minutes");
+            System.out.println("The Total Cost is " + totalCost);
+        }else{
+            System.out.println(totalCost);
+        }
+        
 
         }
 
-
+       
     }
     
 
